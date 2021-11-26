874ac7881569aff5f53446003da3ac753dbce105
Eli Tamakloe <elitamakloe@Elis-MBP.gtas>
2021-11-27 10:23:44 +1100

    Init Commit With Base project Structure

---
+++ app/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+    std::cout << "HID!!" << std::endl;
+    return 0;
+}
---
+++ include/Version/Version.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#define PROJECT_NAME "hidapi C++"
+#define PROJECT_VERSION "0.0.1"
+#define API_VERSION "0.0.1"
